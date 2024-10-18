f23301b31e1bede5dbebcf6f43cc8e6753729aff
mohitkothari-dev <mrulekothari@gmail.com>
2024-10-18 15:10:11 -0400

    initial commit

---
+++ drizzle.config.js
@@ -0,0 +1,10 @@
+/** @type { import("drizzle-kit").Config } */
+export default {
+    schema: "./utils/schema.tsx",
+    dialect: 'postgresql',
+    dbCredentials: {
+      url: 'postgresql://ai-interview-mocker_owner:CaXRTHu1cpj8@ep-empty-wind-a598we22.us-east-2.aws.neon.tech/AI-Content-Generator?sslmode=require',
+
+    }
+  };
+  
